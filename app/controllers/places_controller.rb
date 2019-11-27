4ed5674ba63ed35a39936abbf6de6a81a1d03aff
Heni <henockw@gmail.com>
2019-11-27 08:04:05 -0800

    step 1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,7 @@ class PlacesController < ApplicationController
 
 
   def index
+    @places = Place.all #all the places in our database are going to live in this variable called @places
   end
-  
+
 end
